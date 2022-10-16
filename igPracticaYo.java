b6632873bffbcd2389fabf15eae3738741011eb1
yahir140306 <95980218+yahir140306@users.noreply.github.com>
2022-10-16 00:01:24 -0500

    Delete igPracticaYo.java

--- igPracticaYo.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class igPracticaYo {
-    public static void main (String [] args) {
-        System.out.println (" Hola Por favor ingrese su edad: ");
-        Scanner sc = new Scanner(System.in);
-    }
-}
